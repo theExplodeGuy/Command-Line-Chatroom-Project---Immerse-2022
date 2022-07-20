908fa9f5e70659e534c3c5c1bf68307ccbb5e0a7
benwalsh88 <benjiboss1987@gmail.com>
2022-07-20 14:30:32 +0100

    Update Client.py

---
+++ Client.py
@@ -0,0 +1,32 @@
+from ipaddress import ip_address
+import socket, sys, select
+port = 8080
+connect_address = input("Enter IP Address\n")
+
+ip_list = connect_address.split('.')
+if len(ip_list) == 4:
+    for x in ip_list:
+        if int(x) > 255 or int(x) < 0:
+            print("that's not how IPs work my guy . . . ")
+            exit()
+else:
+    print("Study ur networking :/")
+    exit()
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server.connect((ip_address,port))
+
+while True:
+    sockets_list = [sys.stdin, server]
+    read_sockets,write_socket, error_socket = select.select(sockets_list,[],[])
+
+    for socks in read_sockets:
+            if socks == server:
+                message = socks.recv(2048)
+                print (message)
+            else:
+                message = sys.stdin.readline()
+                server.send(message)
+                print(message)
+server.close()
